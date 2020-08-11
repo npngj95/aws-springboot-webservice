17cc9ad1919b6ed25be5e6e290f26ccc0e8675ff
npngj95 <npngj95@naver.com>
2020-08-11 14:07:37 +0900

    5장 - GooGle 로그인 완료

--- src/main/java/com/npngj95/book/springboot/web/IndexController.java
+++ src/main/java/com/npngj95/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.npngj95.book.springboot.web;
 
+import com.npngj95.book.springboot.config.auth.dto.SessionUser;
 import com.npngj95.book.springboot.service.PostsService;
 import com.npngj95.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,14 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.finAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
@@ -33,5 +43,7 @@ public class IndexController {
 
         return "posts-update";
     }
+
+
 
 }
